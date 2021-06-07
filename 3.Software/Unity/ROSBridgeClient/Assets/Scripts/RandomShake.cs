b79b8cc0a1c2cffb1e1c3208f235544f5169b307
稚晖 <593245898@qq.com>
2021-06-07 23:40:31 +0800

    [Hw] Remove servo interface.

---
+++ 3.Software/Unity/ROSBridgeClient/Assets/Scripts/RandomShake.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomShake : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Quaternion start, end;
+    private int tick;
+
+    void Start()
+    {
+        start = transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tick++;
+        end = start * Quaternion.Euler(new Vector3(0, 0, Random.Range(-1, 1)));
+
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, end, 0.5f);
+
+        transform.localPosition -= new Vector3(0, 0, 0.01f + 0.001f * Random.Range(-1, 1));
+    }
+}
